4e42c2af827a53ee235f5aa7c3fb5fdcfeebc007
chenyulue <chenyulue@163.com>
2024-12-15 20:58:28 +0800

    add NLPModel

--- src/helper/models/NLPModel.py
+++ src/helper/models/NLPModel.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import hanlp
 
 from typing import TypeAlias
@@ -12,6 +13,7 @@ class NLPModel:
 
         self.tokenizer = hanlp.load(hanlp.pretrained.tok.COARSE_ELECTRA_SMALL_ZH)
         self.tokenizer.config.output_spans = True
+        self.tokenizer.dict_combine = {"所述", "所述的"}
 
         self.pos_tagger = hanlp.load(hanlp.pretrained.pos.CTB9_POS_ELECTRA_SMALL)
         self.dep_parser = hanlp.load(hanlp.pretrained.dep.CTB9_DEP_ELECTRA_SMALL)
@@ -29,12 +31,46 @@ class NLPModel:
     def get_deps(self, tokens: Tokens):
         return self.dep_parser(tokens)
 
-    def extract_terms(self) -> dict[str, Positions]:
+    def extract_terms(self) -> defaultdict[str, list[int]]:
         tokens, positions = self.get_tokens()
-        deps = self.get_deps(tokens)
+        deps = iter(self.get_deps(tokens))
 
-        term_tag = ["nn", "dobj", "nsubj"]
+        term_end_tags = [
+            "nn",
+            "dobj",
+            "pobj",
+            "lobj",
+            "nsubj",
+            "xsubj",
+            "nsubjpass",
+            "top",
+        ]
 
-        for dep in deps:
-            if dep["form"] == "所":
-                pass
+        terms = defaultdict(list)
+
+        while True:
+            try:
+                dep = next(deps)
+                if dep["form"].startswith("所述"):
+                    position = positions[dep["id"] - 1]
+                    term_toks = []
+
+                    term_dep = next(deps)
+                    while True:
+                        if term_dep["id"] <= dep["head"]:
+                            term_toks.append(term_dep)
+                        elif term_dep["head"] == term_toks[-1]["head"]:
+                            term_toks.append(term_dep)
+                        elif term_dep["deprel"] in term_end_tags and term_dep["id"] == term_toks[-1]["head"]:
+                            term_toks.append(term_dep)
+                        else:
+                            break
+                        term_dep = next(deps)
+
+                    term = "".join(d["form"] for d in term_toks)
+                    terms[term].append(position[0])
+                    print(term, "at", position)
+            except StopIteration:
+                break
+
+        return terms
